fad55ef070dce22f9067fa841ae0fbeff419aa78
Paige Stephenson <paigestephenson29@gmail.com>
2023-03-09 11:24:48 -0700

    Add files and storyboard for ISpy Lab

--- ISpy/ViewController.swift
+++ ISpy/ViewController.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        scrollView.delegate = self
+       
     }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        scrollView.zoomScale = scale
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateZoomFor(size: view.bounds.size )
+    }
+    
+
+    
 
 
 }
